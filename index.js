05ad5063bf88a2d083f0083e7af763286dfd9b9e
Musthak <musthakcool55@gmail.com>
2026-07-15 07:45:07 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let students = 30;
+
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 2;
+students = students ** 2;
+let extraStudents = students % 3;
+
+console.log(students);
