6b4ed7bf25fc0af36fe0e050f511eedb7c3ce830
Musthak <musthakcool55@gmail.com>
2026-07-16 08:03:56 +0530

    math objects

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const pi = 3.14159;
-let radius;
-let circumference;
+let x = 3.21;
+let y = 2;
+let z;
 
-document.getElementById('mySubmit').onclick = function() {
-    radius = document.getElementById('myText').value;
-    radius = Number(radius);
-    circumference = 2 * pi * radius;
-    document.getElementById('myH3').textContent = circumference;
-}
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.trunc(x);
+z = Math.pow(x, 2);
+z = Math.sqrt(x);
+z = Math.sin(x);
+z = Math.cos(x);
+z = Math.tan(x);
+z = Math.abs(x);
+z = Math.sign(x);
+let max = Math.max(x, y, z);
+let min = Math.min(x, y, z);
